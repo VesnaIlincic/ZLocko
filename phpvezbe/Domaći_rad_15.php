a4c0dd44baf8e6b6f4812f5aec45030a811e4d47
VesnaIlincic <ilincic.vesna@gmail.com>
2019-07-23 22:53:01 +0200

    doterivanje stampanja

--- phpvezbe/Domaći_rad_15.php
+++ phpvezbe/Domaći_rad_15.php
@@ -5,6 +5,7 @@ koji sadrži samo pozitivne brojeve.*/
 
 $a=array(-2, 4, -67, 34, -34, -23);
 $lena=count($a);
+echo "Pozitivni brojevi datog celobrojnog niza su: <br>";
 for($i=0;$i<$lena-1;$i++)
 {
     if($a[$i]>=0)
@@ -18,10 +19,11 @@ echo "<br>";
 /*Zad.21. Dati su nizovi $a[0], $a[1], …, $a[n - 1] i  $b[0], $b[1], …, $b[n - 1]. 
 Formirati niz $c[0], $c[1], …, $c[n – 1] čiji su elementi $a[0] * $b[0], $a[1] * $b[1], …, $a[n – 1] * $b[n – 1].*/
 
-$a1=array(2, -45, 76, 78, -89);
-$b1=array(32, -16, 17, -77, 123);
+$a1=array(2, -15, 7, 18, -9);
+$b1=array(3, -3, 17, -7, 12);
 $n=count($a1);
 $c1=array();
+echo "Clanovi novodobijenog niza su: <br>";
 for($i=0;$i<$n;$i++)
 {
     $c1[$i]=$a1[$i]*$b1[$i];
